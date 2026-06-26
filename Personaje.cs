fc59df784fe837d38ba6581c33877dd1e71dee4d
Grillo <s.deleonmanuel@gmail.com>
2026-06-26 11:22:36 -0300

    Repaso de propiedades y métodos

---
+++ Personaje.cs
@@ -0,0 +1,34 @@
+public class Personaje
+{
+    // Campo privado, solo esta clase puede modificarlo directamente
+    private int puntosDeVidaActuales;
+
+    // Propiedades públicas, la forma "correcta" de exponer datos hacia afuera de la clase
+    public string Nombre { get; set; }
+    public int PuntosDeVidaMaximos { get; set; }
+
+    // Función Constructor: se ejecuta una sola vez cuando  creamos el objeto con new
+    public Personaje(string nombre, int puntosDeVidaMaximos)
+    {
+        Nombre = nombre;
+        PuntosDeVidaMaximos = puntosDeVidaMaximos;
+        puntosDeVidaActuales = puntosDeVidaMaximos; // Arranca con la vida al maximo
+    }
+
+    public void RecibirDanio(int cantidadDeDanio)
+    {
+        puntosDeVidaActuales -= cantidadDeDanio;
+
+        if (puntosDeVidaActuales < 0)
+        {
+            puntosDeVidaActuales = 0;
+        }
+
+        Console.WriteLine($"{Nombre} -> recibió {cantidadDeDanio} de daño. Vida actual {puntosDeVidaActuales}/{PuntosDeVidaMaximos}");
+    }
+
+    public void MostrarEstado()
+    {
+        Console.WriteLine($"{Nombre} -> {puntosDeVidaActuales}/{PuntosDeVidaMaximos} HP");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Personaje heroe  = new Personaje ("Lyra", 100);
+
+heroe.MostrarEstado();
+heroe.RecibirDanio(30);
+heroe.RecibirDanio(50);
+heroe.MostrarEstado();
